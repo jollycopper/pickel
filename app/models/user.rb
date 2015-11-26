34b39678c83769479908a5fdbde8ecc3d23f6700
Ian Wen <happyboylxy@gmail.com>
2015-11-26 15:45:42 +0800

    Add password authentication to User
    Follow guide in
    https://www.railstutorial.org/book/modeling_users#cha-6_footnote-ref-22

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
+  # Only save downcase email
+  before_save { self.email = email.downcase }
   # name must be present
   validates :name,  presence: true, length: { maximum: 50 }
   # Regex for validating email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: { case_sensitive: false }
+  # Add password digest to the model
+  has_secure_password
+  validates :password, presence: true, length: { maximum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125131538) do
+ActiveRecord::Schema.define(version: 20151126073026) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
