ce8ddd9c5744351f88022b1cb7c3d87765011c7a
Ian Wen <happyboylxy@gmail.com>
2015-12-04 16:08:11 +0800

    Nits about search

--- app/controllers/course_select_controller.rb
+++ app/controllers/course_select_controller.rb
@@ -1,13 +1,12 @@
 class CourseSelectController < ApplicationController
   def search
     if params[:query]
-      @courses = Course.where("title LIKE ?", "%#{params[:query][:query_str]}%").paginate(page: params[:page], per_page: 8)
+      query_str = "\"%#{params[:query][:query_str]}%\""
+      @courses = Course
+                     .where("title LIKE #{query_str} or instructor like #{query_str} or description like #{query_str}")
+                     .paginate(page: params[:page], per_page: 8)
     else
       @courses = Course.paginate(page: params[:page], per_page: 8)
     end
-  end
-
-  def query
-
   end
 end
