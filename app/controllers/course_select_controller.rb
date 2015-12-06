ccb6bd304acfd6b992d32be8b82d34abf48825ee
Ian Wen <happyboylxy@gmail.com>
2015-12-06 15:57:39 +0800

    Enable search by year

--- app/controllers/course_select_controller.rb
+++ app/controllers/course_select_controller.rb
@@ -4,8 +4,9 @@ class CourseSelectController < ApplicationController
   def search
     if params[:query]
       query_str = "\"%#{params[:query][:query_str]}%\""
+      query_year = params[:query][:query_str].to_i
       @courses = Course
-                     .where("title LIKE #{query_str} or instructor like #{query_str} or description like #{query_str}")
+                     .where("title LIKE #{query_str} or instructor like #{query_str} or description like #{query_str} or year = #{query_year}")
                      .paginate(page: params[:page], per_page: 8)
     else
       @courses = Course.paginate(page: params[:page], per_page: 8)
